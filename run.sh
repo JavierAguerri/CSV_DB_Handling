d8f2089eb62322063fa12f098e5dff9148826bb3
JavierAguerri <javieraguerrimoreno@gmail.com>
2023-03-13 14:27:29 +0100

    DB connection details

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set environment variables
-export DB_URL="jdbc:mysql://localhost:3306/"
-export DB_NAME="ordersDB"
-export DB_USER="javier"
-export DB_PASS="kl3y-i_WMnD3CtpN@B2r-"
+export DB_URL=""
+export DB_NAME=""
+export DB_USER=""
+export DB_PASS=""
 
 # Set working directory to the root of your project
 cd csvdbhandling
